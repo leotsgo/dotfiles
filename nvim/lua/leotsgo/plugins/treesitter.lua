90eb88ee25e501ab444eb13c6e4f87b0a8e69ac0
Leonardo Diniz Bermejo <lbermejo@pagbank.com>
2024-09-10 12:05:32 -0300

    feat(nvim) adds noneckpain plugin

--- nvim/lua/leotsgo/plugins/treesitter.lua
+++ nvim/lua/leotsgo/plugins/treesitter.lua
@@ -10,7 +10,22 @@ return {
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+        ensure_installed = {
+          'bash',
+          'c',
+          'html',
+          'lua',
+          'markdown',
+          'vim',
+          'vimdoc',
+          'yaml',
+          'markdown',
+          'markdown_inline',
+          'json',
+          'javascript',
+          'typescript',
+          'python',
+        },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },

8b47f022ff45d976f8d2175bdf750198f521928b
Leonardo Diniz Bermejo <lbermejo@pagbank.com>
2024-10-10 15:04:21 -0300

    feat(nvim): enable hidden files in oil.nvim

--- nvim/lua/leotsgo/plugins/oil.lua
+++ nvim/lua/leotsgo/plugins/oil.lua
@@ -4,6 +4,10 @@ return {
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('oil').setup()
+    require('oil').setup {
+      view_options = {
+        show_hidden = true,
+      },
+    }
   end,
 }

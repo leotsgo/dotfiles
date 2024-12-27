b0c81e52676c0743a1c23c1210d8761a7e09e144
Leonardo Diniz Bermejo <me@leotsgo.dev>
2024-12-27 17:13:20 -0300

    feat(nvim): adds background transparency to catppuccin theme

--- home/nvim/colorscheme.lua
+++ home/nvim/colorscheme.lua
@@ -4,6 +4,9 @@ return {
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
+    require('catppuccin').setup {
+      transparent_background = true,
+    }
     -- load the colorscheme here
     vim.cmd [[colorscheme catppuccin-macchiato]]
   end,
